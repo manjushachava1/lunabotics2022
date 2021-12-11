12300da7e6d999ac89f1f259dd150ddcbdeaa18b
Mike Rossetti <mfrossetti@wpi.edu>
2021-12-11 18:35:35 -0500

    robot plugins without anything

---
+++ digbot4/plugins/rover.cc
@@ -0,0 +1,37 @@
+#ifndef ROVER_PLUGIN
+#define ROVER_PLUGIN
+
+// Include Gazebo headers.
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+
+// Include ROS headers so we can communicate with our robot
+#include <ros/ros.h>
+
+// Include std::string's because they're pretty darn useful.
+#include <string>
+
+namespace gazebo
+{
+  // Defining our plugin class
+  class RoverPlugin : public ModelPlugin
+  {
+    private:
+
+    public:
+    RoverPlugin() {}
+    
+    virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf){
+       if(!ros::isInitialized){
+         ROS_FATAL_STREAM("ROS node for Gazebo not established. Plugin failed.");
+         return;
+       }
+       ROS_INFO("Rover Plugin Loaded");
+    }	
+  };
+
+  // Gazebo macro to set up the rest of the plugin functionality
+  GZ_REGISTER_MODEL_PLUGIN(RoverPlugin)
+}
+
+#endif
